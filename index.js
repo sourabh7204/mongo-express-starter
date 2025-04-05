6abce0aa04cc29d145702a7bd4bd19870a55d384
sourabh7204 <sourabh722004@gmail.com>
2025-04-05 21:49:13 +0530

    Added Get Request and Styles

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const exp = require("constants");
 
 //Connection "views" folder with ejs file
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "Public")));
 
 //Connecting Mongoose with js file
 main()
@@ -20,16 +22,11 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/WhatsApp");
 }
 
-//Inserting data in DB
-let chat1 = new Chat({
-  from: "Sankeerth",
-  to: "Rohith",
-  msg: "Send me DBMS notes",
-  created_at: new Date(), //new Date() creates random date... it is inbuild feature of js
-});
-
-chat1.save().then((res) => {
-  console.log(res);
+//Index Route
+app.get("/chats", async (req, res) => {
+  let Chats = await Chat.find();
+  console.log(Chats);
+  res.render("index.ejs", { Chats });
 });
 
 app.get("/", (req, res) => {
---
+++ init.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main()
+  .then(() => {
+    console.log("Connection Succesful!");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/WhatsApp");
+}
+
+let allChats = [
+  {
+    from: "Sankeerth",
+    to: "Rohith",
+    msg: "Send me DBMS notes",
+    created_at: new Date(),
+  },
+  {
+    from: "aditya",
+    to: "swati",
+    msg: "Can you help me with the presentation?",
+    created_at: new Date(),
+  },
+  {
+    from: "raj",
+    to: "priya",
+    msg: "Did you check the latest update?",
+    created_at: new Date(),
+  },
+  {
+    from: "sanya",
+    to: "vihaan",
+    msg: "Share the project files, please.",
+    created_at: new Date(),
+  },
+  {
+    from: "aakash",
+    to: "isha",
+    msg: "Let’s plan for the weekend outing.",
+    created_at: new Date(),
+  },
+  {
+    from: "meera",
+    to: "rohan",
+    msg: "Can you review my essay?",
+    created_at: new Date(),
+  },
+  {
+    from: "manav",
+    to: "ananya",
+    msg: "Do we have a meeting tomorrow?",
+    created_at: new Date(),
+  },
+  {
+    from: "shivani",
+    to: "vikram",
+    msg: "Kindly approve the budget proposal.",
+    created_at: new Date(),
+  },
+];
+
+Chat.insertMany(allChats);
