7d69d4c617307640c45d5f7a47307fdb9eaba2b5
sourabh7204 <sourabh722004@gmail.com>
2025-04-06 22:13:42 +0530

    Customized Edit Option

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 // View engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -54,9 +56,25 @@ app.post("/chats/new", async (req, res) => {
 
 //Chat Edit Route
 app.get("/chats/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  let chat = await Chat.findById(id);
-  res.render("edit.ejs", { chat });
+  const { id } = req.params;
+  const chat = await Chat.findById(id);
+  res.render("edit", { chat });
+});
+
+//Update Route
+app.put("/chats/:id", async (req, res) => {
+  const { id } = req.params;
+  const { msg: newMsg } = req.params;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { msg: newMsg },
+    {
+      runValidators: true,
+      new: true,
+    }
+  );
+  console.log(updatedChat);
+  res.redirect("/chats");
 });
 
 // Start server
